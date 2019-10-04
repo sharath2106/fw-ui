9db8ce308fb2a0e84fea42b76d9b0f900d6ecf8d
Karen <kaskar2008@mail.ru>
2019-10-04 03:13:44 +0300

    Update [FwInput class] fw-input
    - Add mutable prop `value`
    - Add handlers for `click` and `change` events

--- src/components/fw-input/fw-input.tsx
+++ src/components/fw-input/fw-input.tsx
@@ -12,6 +12,16 @@ export class FwInput {
   @Prop() required: boolean;
   @Prop() stateText: string;
 
+  @Prop({ mutable: true }) value: string;
+
+  handleChange(ev: Event) {
+    this.value = ev.target ? (ev.target as HTMLInputElement).value : null;
+  }
+
+  handleClearClick() {
+    this.value = '';
+  }
+
   render() {
     return <div class="input">
       <div class='label'>{this.title}{this.required?<span class="required">*</span>:''} </div>
